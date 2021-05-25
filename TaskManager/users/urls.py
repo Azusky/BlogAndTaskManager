05660cf79ae7f2f9b80e0f46fe902deaf55fe94f
Azusky <68742696+Azusky@users.noreply.github.com>
2021-05-25 14:13:09 +0300

    Update urls.py

--- TaskManager/users/urls.py
+++ TaskManager/users/urls.py
@@ -1,15 +1,3 @@
-
-# from django.urls import path, include
-# from users import views
-
-
-# urlpatterns = [
-#     path('users/', views.UserList.as_view()),
-#     # path('users/', views.users_list),
-
-#     path('users/<int:pk>/', views.UserDetail.as_view()),
-# ]
-
 from django.urls import path
 from .views import MyObtainTokenPairView, RegisterView, UserList
 from rest_framework_simplejwt.views import TokenRefreshView
